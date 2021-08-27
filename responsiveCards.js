f5ac66e9251a52fa9562ae7990c30a9682aacf8a
bhanuteja <bhanutejasangars@gmail.com>
2021-08-28 00:29:01 +0530

    First commit

---
+++ responsiveCards.js
@@ -0,0 +1,15 @@
+
+function getJokes(){
+fetch("https://official-joke-api.appspot.com/random_ten")
+.then((Response)=>{
+return Response.json()
+}).then((data)=>{
+
+  for(i=0;i<10;i++){
+  document.getElementById("type"+i).innerText=data[i].type;
+  document.getElementById("setup"+i).innerText=data[i].setup;
+  }
+})}
+window.onload=function(){
+  getJokes()
+}
