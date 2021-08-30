5fd7adb6baefae04de1e85ba59054e1e041cb966
bhanuteja <bhanutejasangars@gmail.com>
2021-08-30 17:04:29 +0530

    Search button is included

--- responsiveCards.js
+++ responsiveCards.js
@@ -2,7 +2,7 @@
 function getJokesnow(){
 fetch("https://official-joke-api.appspot.com/random_ten")
 .then((Response)=>{
-return Response.json()
+return Response.json() 
 }).then((data)=>{
 
   for(i=0;i<10;i++){
@@ -12,4 +12,23 @@ return Response.json()
 })}
 window.onload=function(){
   getJokesnow()
+  
 }
+function myFunction() {
+  var input=document.getElementById('myInput').value.toUpperCase();
+  var  cardcontainer=document.getElementById('cardcontainer');
+  var cards=cardcontainer.getElementsByClassName('card');
+  
+ for(let i=0;i<cards.length;i++){
+  let title=cards[i].querySelector('#cardTitle')
+  console.log(title)
+  if(title.innerText.toUpperCase().indexOf(input)>-1){
+    cards[i].style.display ="";
+  }else{
+    cards[i].style.display ="none";
+  }
+ }}
+
+/*
+
+ } */
